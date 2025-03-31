4a896e29e95bb99cddc23f9870e12ad1b2231e8e
PeterM739 <masic.peter@gmail.com>
2025-03-31 16:37:10 +0200

    navodila in nekaj malega kode

--- model.jl
+++ model.jl
@@ -1,1 +1,14 @@
+using LinearAlgebra
+using Plots
+
 print("Radarji")
+
+u = [1, 0, -1, 0]
+v = [0, -1, 0, 1]
+
+function signalStrength(x, y, u, v)
+    return sum(1 / ((x - u[i])^2 + (y - v[i])^2) for i in eachindex(u))
+end
+
+signalStrength(1, 2, u, v)
+
